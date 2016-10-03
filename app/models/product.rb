359de53838ab67e9b76f6cec914ef0a27f6da764
gitpetr <pkhilenko@yandex.ru>
2016-10-03 09:23:00 +0600

    add assets

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,16 @@ class Product < ActiveRecord::Base
   validates :title, :description,  presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  has_many :line_items, dependent: :destroy
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
+    end
 end
